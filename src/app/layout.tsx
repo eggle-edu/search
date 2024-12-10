24dff725db17587bb20beefafdad9478e7a0aa8a
minkithub <atunh9988@naver.com>
2024-12-10 11:10:49 +0900

    tailwind, pretendard 폰트 적용.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import { Provider } from "@/components/ui/provider"
+import localFont from 'next/font/local'
+
+const pretendard = localFont({
+  src: '../fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+})
+
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
   return (
     <html suppressHydrationWarning>
-      <body>
+      <body className={`${pretendard.variable} font-pretendard`}>
         <Provider>{children}</Provider>
       </body>
     </html>
